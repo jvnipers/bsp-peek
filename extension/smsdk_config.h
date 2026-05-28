1c649e7d2b3ed76e3ec594c4eb28a818d70918f4
Dots <dotvnl@gmail.com>
2026-05-28 19:17:00 +0300

    bump ver

--- extension/smsdk_config.h
+++ extension/smsdk_config.h
@@ -5,7 +5,7 @@
 #define SMEXT_CONF_DESCRIPTION                                                 \
   "\"Peek\" into BSP engine and file data (brushes, leaves, planes, nodes, "   \
   "displacements)"
-#define SMEXT_CONF_VERSION "1.3.0"
+#define SMEXT_CONF_VERSION "1.3.1"
 #define SMEXT_CONF_AUTHOR "jvnipers"
 #define SMEXT_CONF_URL "https://github.com/jvnipers/bsp-peek"
 #define SMEXT_CONF_LOGTAG "BSPP"
